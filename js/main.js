fad92bae12ff68d955329cc2b34036f6bf093df9
Engineer Rasel <59188760+engrasel@users.noreply.github.com>
2021-06-25 16:22:03 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Params
+var sliderSelector = '.project_slider',
+    options = {
+      init: false,
+      loop: true,
+      speed:500,
+      slidesPerView: 2, // or 'auto'
+      // spaceBetween: 10,
+      centeredSlides : true,
+      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 50, // Slide rotate in degrees
+        depth: 0, // Depth offset in px (slides translate in Z axis)
+        modifier: 1, // Effect multipler
+        slideShadows : true, // Enables slides shadows
+      },
+      grabCursor: true,
+      parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        // when window width is <= 320px     
+      768: {       
+         slidesPerView: 1,
+         spaceBetween: 10     
+      }, 
+        1023: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      },
+      // Events
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+var mySwiper = new Swiper(sliderSelector, options);
+
+// Initialize slider
+mySwiper.init();
